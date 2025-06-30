c88cf2f4c627513e17b076963420642687e7fb05
NotTheTorch <vishnutejashanmukha928@gmail.com>
2025-06-30 14:50:20 +0530

    feat: Added Iterative Implementation

--- Trees/BinaryTreeMax.js
+++ Trees/BinaryTreeMax.js
@@ -1,3 +1,34 @@
+class Queue
+{
+    constructor()
+    {
+        this.arr = [];
+        this.front = -1;
+        this.end = -1;
+    }
+
+    enqueue(x)
+    {
+        if(this.arr.length === 0)
+        {
+            this.arr.push(x);
+            this.front = 0;
+            this.end = 0;
+            return;
+        }
+        this.arr.push(x);
+        this.end++;
+        return;
+    }
+
+    dequeue()
+    {
+        let result = this.arr[this.front];
+        this.front++;
+        return result;
+    }
+}
+
 class Node
 {
     constructor(data)
@@ -15,7 +46,7 @@ root.right = new Node(80);
 root.right.left = new Node(5);
 
 
-//Recursive Method Implementation
+//Recursive Implementation
 
 function getMax(root)
 {
@@ -25,4 +56,27 @@ function getMax(root)
     return Math.max(root.data,getMax(root.left),getMax(root.right));
 }
 
-console.log(getMax(root));
+
+//Iterative Implementation
+
+function iterative_getMax(root)
+{
+    const queue = new Queue();
+    let maxValue = Number.NEGATIVE_INFINITY;
+    queue.enqueue(root);
+
+    while(queue.front <= queue.end)
+    {
+        let curr = queue.dequeue();
+        maxValue = Math.max(maxValue,curr.data);
+
+        if(curr.left)
+            queue.enqueue(curr.left);
+
+        if(curr.right)
+            queue.enqueue(curr.right)
+    }
+    return maxValue;
+}
+
+console.log(iterative_getMax(root));
