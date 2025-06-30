987ed97165d5e4f35b34b8b0e18239ecd44f3d89
NotTheTorch <vishnutejashanmukha928@gmail.com>
2025-06-30 17:33:43 +0530

    Initial Commit

---
+++ Trees/sizeOfBinaryTree.js
@@ -0,0 +1,46 @@
+class Queue
+{
+    constructor()
+    {
+        this.arr = [];
+        this.front = -1;
+        this.end = -1;
+    }
+
+    enqueue(x)
+    {
+        if(this.arr.length === 0)
+        {
+            this.arr.push(x);
+            this.front = 0;
+            this.end = 0;
+            return;
+        }
+        this.arr.push(x);
+        this.end++;
+        return;
+    }
+
+    dequeue()
+    {
+        let result = this.arr[this.front];
+        this.front++;
+        return result;
+    }
+}
+
+class Node
+{
+    constructor(data)
+    {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+let root = new Node(10);
+root.left = new Node(20);
+root.right = new Node(80);
+root.right.left = new Node(5);
