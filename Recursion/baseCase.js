6848fd42fe910798daa32c2f2f18ebdae5fd5157
NotTheTorch <vishnutejashanmukha928@gmail.com>
2025-07-02 19:37:30 +0530

    feat: Recursive Implementation on factorial of a number

---
+++ Recursion/baseCase.js
@@ -0,0 +1,11 @@
+//Factorial of a given number recursive implementation
+
+function factorial(n)
+{
+    if(n === 0 || n === 1)
+        return 1;
+
+    return n * factorial(n-1);
+}
+
+console.log(factorial(5));
