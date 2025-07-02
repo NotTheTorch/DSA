6ebb4a71e9c7a70c49057e9d1e2aa2364d2b24d4
NotTheTorch <vishnutejashanmukha928@gmail.com>
2025-07-02 19:42:29 +0530

    feat: added fibonacci recursive implementation

--- Recursion/baseCase.js
+++ Recursion/baseCase.js
@@ -2,10 +2,20 @@
 
 function factorial(n)
 {
-    if(n === 0 || n === 1)
+    if(n <= 0)
         return 1;
 
     return n * factorial(n-1);
 }
 
 console.log(factorial(5));
+
+function fibonacci(n)
+{
+    if(n === 1 || n === 0)
+        return 1;
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+console.log(fibonacci(10));
