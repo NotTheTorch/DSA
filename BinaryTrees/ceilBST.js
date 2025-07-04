f571b1389efa97fb94710150392724d5d705105b
NotTheTorch <vishnutejashanmukha928@gmail.com>
2025-07-04 19:33:06 +0530

    feat: add ceilBST file

---
+++ BinaryTrees/ceilBST.js
@@ -0,0 +1,37 @@
+class Node
+{
+    constructor(data)
+    {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const head = new Node(50);
+head.left = new Node(30);
+head.left.left = new Node(20);
+head.left.right = new Node(40);
+head.right = new Node(70);
+head.right.right = new Node(80);
+head.right.left = new Node(60);
+
+function floor(root,value)
+{
+    let res = null;
+    while(root !== null)
+    {
+        if(root.data === value)
+            return root;
+        
+        else if(root.data < value)
+            root = root.right;
+        
+        else
+        {
+            res = root.data;
+            root = root.left;
+        }  
+    }
+    return res;
+}
