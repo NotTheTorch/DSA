22b5f5c21a5842fe3f3beb983c1897c49655aad2
NotTheTorch <vishnutejashanmukha928@gmail.com>
2025-07-07 23:14:19 +0530

    feat: added basic bit operations program

---
+++ BitManipulation/basicBit.js
@@ -0,0 +1,11 @@
+let x = 1, y = 2;
+
+
+console.log(x & y); //AND Operator
+console.log(x | y); //OR Operator
+console.log(x ^ y); //XOR Operator
+
+
+console.log(~5);
+console.log(x << y);
+console.log(y >> x);
